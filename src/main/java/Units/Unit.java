784bf21171a641bc9247e002750f960bbe604fc4
Joakim <joakifa@stud.ntnu.no>
2022-02-16 10:18:15 +0100

    Laget Unit klassen, ikke laget logikken i getAttackBonus og getResistBonus

--- src/main/java/Units/Unit.java
+++ src/main/java/Units/Unit.java
@@ -1,4 +1,53 @@
 package Units;
 
 public class Unit {
+    private final String name;
+    private int health;
+    protected int attack;
+    protected int armor;
+
+    public Unit(String name, int health, int attack, int armor) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public void attack(Unit opponent){
+        opponent.health = opponent.health - (this.attack + getAttackBonus()) + (opponent.armor + getResistBonus());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name + "/n" + "Health: " + this.health + "/n" + "Attack: " + this.attack + "/n" +
+                "Armor:" + this.armor;
+    }
+
+    public int getAttackBonus(){
+        return 0;
+    }
+
+    public int getResistBonus(){
+        return 0;
+    }
 }
