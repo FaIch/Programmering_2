3f9e169d937e8a5fb4ce7be07f326a0a8b7ed636
Joakim <joakimfalch@outlook.com>
2022-03-28 19:24:38 +0200

    Made JavaDoc for undocumented classes

--- src/main/java/IO/ArmyCSVRead.java
+++ src/main/java/IO/ArmyCSVRead.java
@@ -6,12 +6,36 @@ import Units.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-
+/**
+ * The code is heavily inspired by lecture in file handling in IDATT2001 Example code.
+ */
+/**
+ * The type Army csv read, used for reading and creating an army from a file.
+ */
 public class ArmyCSVRead {
     private static final String DELIMITER = ",";
 
+    /**
+     * Instantiates a new Army csv read.
+     */
     public ArmyCSVRead() {}
 
+    /**
+     * Reads Army from a csv file.
+     * First the method ensures that the path is not only a '.csv' file, but also that it exists,
+     * and that it is not empty.
+     *
+     * Afterwards it uses scanner to register one line of the file at a time. Creating a new Army with the first line
+     * and then adding units all remaining lines in the file. It splits the lines on comma, and checks that the length
+     * of the split array, is 3. If not, the Army is not written correctly in the file, and can therefor not be read.
+     *
+     * Then it checks what type of unit the added unit should be, before adding that unit. Continues as long as the
+     * scanner finds a new line with information in it.
+     *
+     * @param file the file the army should be read from
+     * @return the army that is created upon reading the file
+     * @throws IOException the io exceptions that are thrown if any of the operations fail.
+     */
     public Army readArmyFromCSV(File file) throws IOException {
         if (!file.getName().endsWith(".csv")){
             throw new IOException("Only .csv files are supported");
--- src/main/java/IO/ArmyCSVWrite.java
+++ src/main/java/IO/ArmyCSVWrite.java
@@ -7,12 +7,32 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 
+/**
+ * The code is heavily inspired by lecture in file handling in IDATT2001 Example code.
+ */
+
+/**
+ * The class for writing an Army to a csv file.
+ */
 public class ArmyCSVWrite {
     private static final String NEWLINE = "\n";
     private static final String DELIMITER = ",";
 
+    /**
+     * Instantiates a new Army csv write.
+     */
     public ArmyCSVWrite(){}
 
+    /**
+     * The actual method for writing the army to a file.
+     * Checks that the path of the file is correct, that it is in the correct format. i.e '.csv'
+     * Uses FileWriter to write strings to files. First line being the army name, and the lines after being
+     * a single unit.
+     *
+     * @param army the army that will be written to a file
+     * @param file the file, the army should be written to. Could be an existing file or a new one.
+     * @throws IOException throws IOException if any of the IO operations fail.
+     */
     public static void writeFile(Army army, File file) throws IOException{
         if (!file.getPath().startsWith(FileSystems.getDefault()
                 .getPath("src","main","resources").toString())){
